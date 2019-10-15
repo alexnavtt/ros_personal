a4cd2c6736e282d69f89e8ff342b5df0040346d2
ajn853 <alexnavtt@utexas.edu>
2019-10-14 20:50:05 -0500

    initialize catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexj/ROS/ros_personal/src
+# Build directory: /home/alexj/ROS/ros_personal/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
