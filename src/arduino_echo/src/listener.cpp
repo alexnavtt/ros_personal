0769966dafc35dbb3d6ae25847a1c1c9ac1d1062
ajn853 <alexnavtt@utexas.edu>
2019-10-15 00:25:40 -0500

    Added subscriber and listener

---
+++ src/arduino_echo/src/listener.cpp
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void shoutHeard(const std_msgs::String::ConstPtr& msg)
+{
+    ROS_INFO(msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("yelled words", 1000, shoutHeard);
+
+    ros::spin();
+
+    return 0;
+}
---
+++ src/arduino_echo/src/shout.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "shout");
+    ros::NodeHandle nh;
+
+    ros::Publisher shouting_person = nh.advertise<std_msgs::String>("yelled words",1000);
+
+    ros::Rate loop_rate(0.33);
+
+    while(ros::ok())
+    {
+        std_msgs::String msg;
+        std::stringstream ss;
+        ss << "I'm yelling this!";
+
+        msg.data = ss.str();
+        ROS_INFO("Yelled");
+        shouting_person.publish(msg);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
