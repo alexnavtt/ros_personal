367308ac418c1d94722a38ef7de9d419220d69b7
alexnavtt <alexnavtt@utexas.edu>
2019-10-23 00:32:13 -0500

    Placed image framework in ros node

--- src/racecar/src/show_image.cpp
+++ src/racecar/src/show_image.cpp
@@ -1,21 +1,67 @@
 #include "ros/ros.h"
+#include "std_msgs/Float64MultiArray.h"
 #include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include <math>
+#include <cv_bridge/cv_bridge.h>
+#include <math.h>
+#include <iostream>
 
-void plot_image(const std_msgs::image_transport::ConstPtr& img)
+cv::Point center = cv::Point(0.0, 0.0);
+cv::VideoCapture cap(0);
+
+void update_center(const std_msgs::Float64MultiArray::ConstPtr& center_loc)
 {
-	//Still need to look at image_transport data type
+	center.x = center_loc->data[0];
+	center.y = center_loc->data[1];
 }
 
-int main(argc, argv**)
+void plot_image()
+{
+	//cv::VideoCapture cap(0);
+
+	cv::Mat frame;
+	cap >> frame;
+
+	//Size of the image
+	int x_max = frame.cols;
+        int y_max = frame.rows;
+
+        //Define parameters for a circle to overlay on frame
+        int radius = 50;
+        int linewidth = 1;
+	int linetype = 8;
+	int shift = 0;
+	cv::Scalar color = cv::Scalar(0, 0, 255);
+
+       
+	//Apply circle to frame
+        circle(frame, center, radius, color, linewidth, linetype, shift);
+
+	//Show frame
+	cv::imshow("Video_Feed", frame);
+	cv::waitKey(1);
+}
+
+int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "show_image");
 	ros::NodeHandle nh;
 
-	ros::Subsriber sub = nh.subscribe("rectangle_center_location", 20, plot_image);
+	ros::Subscriber sub = nh.subscribe("center_location", 20, update_center);
+	ros::Rate r(60);
 
-	ros::spin();
+	while(ros::ok())
+	{
+		plot_image();
+		ros::spinOnce();
+		r.sleep();
+
+		/*Escape condition (press ESC)
+                char c=(char)cv::waitKey(25);
+                if (c==27){
+                        return -1;
+                }*/
+
+	}
 
 	return 0;
 }
